2fc1ba81716a3f26fb3b4b5d170e990eb906fe3c
DanilaKholod <43093915+DanilaKholod@users.noreply.github.com>
2018-11-11 23:09:09 +0300

    26-30(не сделано)

---
+++ 2018.11.11.ДЗ-Prime/26-30.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+struct Product
+{
+	unsigned int price;
+	unsigned int weight;
+};
+
+struct Warehouse
+{
+	Product* Goods;
+	unsigned int size;
+};
+
+int main()
+{
+	cout << "¯\_(ツ)_/¯";
+	return 0;
+}
