6d3a70bda571458a9e56ffb3157488f7255b4b26
DanilaKholod <43093915+DanilaKholod@users.noreply.github.com>
2018-11-30 23:27:42 +0300

    Вообще не работала, исправил

--- 2018.11.25.ДЗ-3/6.cpp
+++ 2018.11.25.ДЗ-3/6.cpp
@@ -2,29 +2,30 @@
 no more than O (n * log log n) steps.*/
 #include <iostream>
 using namespace std;
-int main() {
+int main()  {
 	int n;
 	cin >> n;
 	bool *used = new bool[n + 3];
 	int *array = new int[n + 3];
-	used[0] = used[1] = 0;
-	for (int i = 2; i <= n; i++) {
-		array[i] = 0;
-		used[i] = 1;
-	}
-	for (int i = 2; i <= n+2; i++) {
-		if (used[i]) {
-			for (int j = i + i; j <= n+2; j += i) {
-				used[j] = 0;
-				array[j]++;
-				if (j % (i*i) == 0) {
-					array[j]++;
-				}
+	for (int i = 2; i <= n+2; i++)  {
+        used[i] = 1;
+        array[i] = 0;
+    }
+	for (int i = 2; i <= n + 2; i++)    {
+		if (used[i])    {
+			for (int j = i + i; j <= (n + 2); j += i)   {
+			used[j] = 0;
+			int x = j;
+			while ((x % i) == 0)	{
+                    		array[j]++;
+                    		x /= i;
+                    		if (array[j] > 2) break;
+               		 }
 			}
 		}
 	}
-	for (int i = 2; i <= n; i++) {
-		if (used[i] == 1 && (used[i + 2] == 1 || array[i + 2] == 2)) {
+	for (int i = 2; i < n; i++) {
+		if ((used[i]) && ((used[i + 2]) || (array[i + 2] == 2)))    {
 			cout << i << " ";
 		}
 	}
