ad3e64c8cd9a1d9a451bde73a7adf07a77141637
DanilaKholod <43093915+DanilaKholod@users.noreply.github.com>
2018-11-30 22:57:40 +0300

    Исправил ошибку

--- 2018.11.25.ДЗ-3/4.cpp
+++ 2018.11.25.ДЗ-3/4.cpp
@@ -6,14 +6,17 @@ int main() {
 	int n, count;
 	cin >> n;
 	count = n;
-	n += n + n + 1;
+	if (n <= 5) {
+		n += n + n + 1;
+	}
+	n += n;
 	bool *used = new bool[n + 1];
 	used[0] = used[1] = 0;
 	for (int i = 2; i <= n; i++) {
 		used[i] = 1;
 	}
 	for (int i = 2; i <= n; i++) {
-		if (used[i] && i <= (int)sqrt(n)) {
+		if (used[i] && i) {
 			for (int j = i + i; j <= n; j += i) {
 				used[j] = 0;
 			}
