dafa7e85b20ec45c8db9ee60ca6db52ce49e4eee
DanilaKholod <43093915+DanilaKholod@users.noreply.github.com>
2019-03-10 22:49:45 +0300

    Ленин

--- 2019.03.03.ДЗ-5/4.cpp
+++ 2019.03.03.ДЗ-5/4.cpp
@@ -38,7 +38,7 @@ public:
 	void push_index(int x, int j) {
 		int i = 0;
 		Node* temp = new Node;
-		tmp->data = x;
+		temp->data = x;
 		Node* temp = head;
 		while (i != j) {
 			temp = temp->next;
